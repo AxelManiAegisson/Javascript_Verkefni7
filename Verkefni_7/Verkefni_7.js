3dfc0a786570f55e063b3de60d3f9c14d028b932
AxelManiAegisson <33860352+AxelManiAegisson@users.noreply.github.com>
2019-05-08 21:51:09 +0000

    Add files via upload

--- Verkefni_7/Verkefni_7.js
+++ Verkefni_7/Verkefni_7.js
@@ -1,8 +1,38 @@
-fetch('https://apis.is/concerts')
+function createNode(element){
+	return document.createElement(element);
+}
+
+function appen(parent,el){
+	return parent.appendChild(el);
+}
+
+const ul = document.getElementById("syningar");
+
+const url = 'https://apis.is/concerts';
+
+
+
+
+fetch(url)
 	.then((resp) => resp.json())
 	.then(function(data){
+		let syningar = data.results;
 
+		return syningar.map(function(syningar){
+			let li = createNode('li'),
+			img = createNode('img'),
+			span = createNode('span');
+
+			img.src=syningar.picture.medium;
+
+			span.textContent={syningar.eventDateName.first} ${syningar.eventDateName.last};
+
+			append(li,img);
+			append(li,span);
+			append(ul,il);
+
+		})
 	})
 	.catch(function(error){
-
+		console.log(JSON.stringify(error));
 	})
